6cc181d6aac297ae3be940bbc2746ae8596b6220
Probotmaker8 <101275268+Probotmaker8@users.noreply.github.com>
2022-05-19 07:22:29 +0530

    Update varss.py

--- Alita/varss.py
+++ Alita/varss.py
@@ -1,6 +1,6 @@
 from os import getcwd
-
-from import prettyconf.loaders import EnvFile,Environment
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
 
 env_file = f"{getcwd()}/.env"
 config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
